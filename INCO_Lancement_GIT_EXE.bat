b59561cd57148bb5643f1ca5e306a5a15fe3153e
feki-ab <60355850+feki-ab@users.noreply.github.com>
2020-01-27 17:04:19 +0100

    Create INCO_Lancement_GIT_EXE.bat

---
+++ INCO_Lancement_GIT_EXE.bat
@@ -0,0 +1,21 @@
+set Environnement=RECETTE
+
+set PathSaveReport=Z:\Inco\
+set currentpath=%~dp0%debug
+
+cd C:\Ranorex\Automates\Inco
+rmdir "incoplu" /s /q
+git clone https://bitbucket.agilefabric.fr.carrefour.com/scm/qod/incoplu.git 
+cd incoplu
+
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /p:Configuration=Debug /t:Rebuild ..\incoplu\INCO\INCO.csproj
+c:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /p:Configuration=Debug /t:Rebuild ..\incoplu\INCO.sln
+
+cd ../Dossier_Lancement_EXE
+
+rmdir "Debug" /s /q
+xcopy "..\incoplu\INCO\bin\Debug" "Debug\" /e /i
+c:
+
+cd %currentpath%
+INCO.exe /ts:INCO.rxtst /rf:%PathSaveReport%\Report_%Environnement%_%%S_%%Y%%M%%D_%%T.html
